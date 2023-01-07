0fe86885c56a6bcee9b11ecbd5248c6f4373796a
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-07 20:39:00 +0530

    routes added

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const cors = require('cors');
 const dbConnection = require('./configuration/connect');
 const signupRoute = require('./controllers/register.controller');
 const loginRoute = require('./controllers/login.controller');
-
+const categoryRoute = require('./controllers/category.controllers');
+const songsRoute = require('./controllers/songs.controllers');
+const premiumRoute = require('./controllers/premium.controllers');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(signupRoute);
 app.use(loginRoute);
+app.use(categoryRoute);
+app.use(songsRoute);
+app.use(premiumRoute);
 
 dbConnection().then(()=>{
     app.listen(3000,()=>{
