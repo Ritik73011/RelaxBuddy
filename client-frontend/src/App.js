da409cd39dc606ee7da24a23be5f6a05038b0e6c
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-09 17:04:49 +0530

    some files added

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
 import ResponsiveDrawer from './components/Sidebar/Sidebar';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { createContext, useMemo, useState } from 'react';
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+  
   return (
     <div className="App">
-      <ResponsiveDrawer/>
+       <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <ResponsiveDrawer/>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
     </div>
   );
 }
