f740f534d16293d3153c93169a67cff6e92eb0c6
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-11 02:14:26 +0530

    removing song from favorite section

--- controllers/favorite.controllers.js
+++ controllers/favorite.controllers.js
@@ -8,6 +8,11 @@ route.post('/favorite',getUserId,async(req,res)=>{
     const uid = req.user_id;
     const {url,poster,title,singer,category} = req.body;
     try {
+        const avilable = await favSongs.findOne({title:title});
+        if(avilable)
+        return res.status(200).send({
+            message:"already added.."
+        })
         const obj = {
             userId:uid,
             url:url,
@@ -43,10 +48,14 @@ route.get('/favorite',getUserId,async(req,res)=>{
 });
 
 //delete user favorite songs
-route.delete('/favorite/:id',async(req,res)=>{
+route.delete('/favorite/:id',getUserId,async(req,res)=>{
     const id = req.params.id;
+    console.log(id);
+    const uid = req.user_id;
     try {
-        await favSongs.findByIdAndDelete(id);
+        let song = await favSongs.findOne({userId:uid,title:id});
+        await favSongs.deleteOne(song._id);
+        console.log(song);
         return res.status(200).send({
             message:"removed from your favorites..."
         })
@@ -57,12 +66,17 @@ route.delete('/favorite/:id',async(req,res)=>{
     }
 })
 //to get song by id to play
-route.get('/favorite/:id',async(req,res)=>{
+route.get('/favorite/:id',getUserId,async(req,res)=>{
     const id = req.params.id;
+    const uid = req.user_id;
     try {
-        const song = await favSongs.findOne({_id:id});
+        const song = await favSongs.find({userId:uid});
+        if(song)
         return res.status(200).send({
             songs:song
+        })
+        return res.status(200).send({
+            flag:false
         })
     } catch (error) {
         return res.status(500).send({
