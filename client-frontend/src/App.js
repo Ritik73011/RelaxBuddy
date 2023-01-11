e4a23a81b59108033891cb7e4edbfa837278061c
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-01-11 13:21:25 +0530

    updated

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import ResponsiveDrawer from './components/Sidebar/Sidebar';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { createContext, useMemo, useState } from 'react';
-import { Route, Routes } from 'react-router';
+import { createContext, useEffect, useMemo, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router';
 import SongContext from './Context/SongContext';
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 function App() {
 
   const [mode, setMode] = useState('light');
+  const navigate = useNavigate();
   const colorMode = useMemo(
     () => ({
       toggleColorMode: () => {
@@ -28,20 +29,27 @@ function App() {
   );
 
   const [url,setUrl] = useState('');
+  const [premiumS,setPremiumS] = useState(null);
 
   const updateUrl = (url)=>{
     setUrl(url);
   }
-  
+  const updatePremiumS = (val)=>{
+    setPremiumS(val)
+  }
+  useEffect(()=>{
+    navigate('/trending')
+  },[])
   return (
     <div className="App">
        <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <SongContext.Provider value={{url,updateUrl}}>
+        <SongContext.Provider value={{url,updateUrl,updatePremiumS,premiumS}}>
 
       <Routes>
-        <Route path='/' element={<ResponsiveDrawer/>}/>
+        <Route path='/:trending' element={<ResponsiveDrawer/>}/>
         <Route path='/songs/:id' element={<ResponsiveDrawer/>}/>
+        <Route path='/songs/premium/:premium' element={<ResponsiveDrawer/>}/>
       </Routes>
         </SongContext.Provider>
       </ThemeProvider>
